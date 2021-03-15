50822828d6ff6b0e962eb341ef357e6ce393bd4a
arabellak <barbaracalle10i3@gmail.com>
2021-03-15 14:56:44 -0500

    Type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Type annotations"""
+
+
+from typing import TypeVar, Mapping, Any, Union
+
+
+def safely_get_value(dct: Mapping,
+                    key: Any,
+                    default: Union[TypeVar('T'), None]
+                    ) -> Union[Any, TypeVar('T')]:
+    """Given code"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
