7b0ed22f42148ef677afe960ba33d84339f145c6
arabellak <barbaracalle10i3@gmail.com>
2021-03-15 13:49:53 -0500

    Callable

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Callable
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function"""
+    def multiply(n: float):
+        """Number"""
+        return n * multiplier
+    return multiply
