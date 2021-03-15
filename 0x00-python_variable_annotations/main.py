b43954f26636c4329117d3f9d4a1fb55b80bbebd
arabellak <barbaracalle10i3@gmail.com>
2021-03-15 14:03:54 -0500

    Main

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+element_length =  __import__('9-element_length').element_length
+
+print(element_length.__annotations__)
+
+"""
 make_multiplier = __import__('8-make_multiplier').make_multiplier
 print(make_multiplier.__annotations__)
 fun = make_multiplier(2.22)
 print("{}".format(fun(2.22)))
+"""
 
 """
 to_kv = __import__('7-to_kv').to_kv
