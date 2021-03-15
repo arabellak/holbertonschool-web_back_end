b4741405fa05a2dd4615de543ba7655f77c1f701
arabellak <barbaracalle10i3@gmail.com>
2021-03-15 14:46:13 -0500

    Pycode happy

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,7 +5,7 @@
 from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) ->  Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Duck-type"""
     if lst:
         return lst[0]
