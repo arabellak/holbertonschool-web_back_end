30d57ae2e2255edce4798e06253b66e4d40c15d4
arabellak <barbaracalle10i3@gmail.com>
2021-03-15 14:12:38 -0500

    Imported List and Union and implemented them on the function

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,9 +2,9 @@
 """Mixed list"""
 
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns their sum as a float"""
     return sum(mxd_lst)
