9fa491c56850a9eda6e945a46dfcdf6c40c2cfec
arabellak <barbaracalle10i3@gmail.com>
2021-03-17 15:52:03 -0500

    Main

---
+++ 0x02-python_async_comprehension/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import asyncio
+measure_runtime = __import__('2-measure_runtime').measure_runtime
+
+
+async def main():
+    return await(measure_runtime())
+
+print(
+    asyncio.run(main())
+)
+
+
+"""
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def main():
+    print(await async_comprehension())
+
+asyncio.run(main())
+"""
+
+"""
+async_generator = __import__('0-async_generator').async_generator
+
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+
+asyncio.run(print_yielded_values())
+"""
