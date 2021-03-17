736ac20f4790b89d524228d6f2538069a4ec3684
arabellak <barbaracalle10i3@gmail.com>
2021-03-17 15:49:54 -0500

    Changed the return value to a list

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
 import asyncio
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
-    """Collects 10 randome numbers using an async comprehension"""
+async def async_comprehension() -> List[float]:
+    """Collects 10 random numbers using an async comprehension"""
     return [i async for i in async_generator()]
