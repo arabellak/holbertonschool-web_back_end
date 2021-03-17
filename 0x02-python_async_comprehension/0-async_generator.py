db3a51acac01412be601067782ddbd923d6f628a
arabellak <barbaracalle10i3@gmail.com>
2021-03-17 15:03:04 -0500

    Pycode happy

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,4 +8,4 @@ async def async_generator():
     """Loops 10 times"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,10)
+        yield random.uniform(0, 10)
