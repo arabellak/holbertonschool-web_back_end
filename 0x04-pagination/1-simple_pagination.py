d87ef17949da3580dbed6f3b4285ecd0931827a0
arabellak <barbaracalle10i3@gmail.com>
2021-03-24 18:49:24 -0500

    Pycode happy

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -9,6 +9,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Contains the star and end index"""
     return (((page - 1) * page_size), (page * page_size))
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -29,9 +30,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Simple pagination"""
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
 
         ir = index_range(page, page_size)
-        
+
         return self.dataset()[ir[0]:ir[1]]
