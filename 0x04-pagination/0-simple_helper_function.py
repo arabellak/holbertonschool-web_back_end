157535d5d61408365e16b0badb4f85030a08780e
arabellak <barbaracalle10i3@gmail.com>
2021-03-23 20:30:04 -0500

    Returns a tuple

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Contains the star and end index"""
+    return ((page - 1), (page_size - 1))
+    
