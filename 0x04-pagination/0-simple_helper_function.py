6e8e98c856f6a41289a7d0f411cdbedfd2b4b42d
arabellak <barbaracalle10i3@gmail.com>
2021-03-24 15:03:01 -0500

    Changed the return value

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -5,4 +5,4 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Contains the star and end index"""
-    return ((page - 1), (page_size - 1))
+    return (((page - 1) * page_size), (page * page_size))
