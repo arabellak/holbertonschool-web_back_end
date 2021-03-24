20c154004a9355664a6c3fbfd7a9bf580babf4bb
arabellak <barbaracalle10i3@gmail.com>
2021-03-24 18:47:27 -0500

    Simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Contains the star and end index"""
+    return (((page - 1) * page_size), (page * page_size))
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        ir = index_range(page, page_size)
+        
+        return self.dataset()[ir[0]:ir[1]]
