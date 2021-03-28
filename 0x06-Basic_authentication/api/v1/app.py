5804da2661035007167c15c7cf33e19b2026b7f7
arabellak <barbaracalle10i3@gmail.com>
2021-03-27 21:10:30 -0500

    Fobidden status

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -20,10 +20,17 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-@app.errorhandler(401):
+@app.errorhandler(401)
 def unauthorized_request(error) -> str:
-    """Unauthorized request"""
+    """Unauthorized request
+    """
     return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """User is authenticate but not allowed to access
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
