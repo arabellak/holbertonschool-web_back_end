de9e347db26c777d5b99e9b5758b72566a7bbb34
arabellak <barbaracalle10i3@gmail.com>
2021-03-29 18:09:24 -0500

    Donde again

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -7,13 +7,12 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -21,17 +20,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
-def unauthorized_request(error) -> str:
-    """Unauthorized request
+def unauthorized(error) -> str:
+    """ Unauthorized user
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """User is authenticate but not allowed to access
-    """
-    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
