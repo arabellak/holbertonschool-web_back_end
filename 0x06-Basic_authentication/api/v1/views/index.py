5cbc6bd146eb9171a7edda07bc65656f56b1ce70
arabellak <barbaracalle10i3@gmail.com>
2021-03-29 17:45:18 -0500

    Pycode happy

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -7,27 +7,30 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
       - the status of the API
     """
     return jsonify({"status": "OK"})
 
-@app_views.route('/unauthorized',strict_slashes=False)
+
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
-  """ GET api/v1/unautorized_status
-  Raise:
-    - 401 error
-  """
-  abort(401)
+    """GET api/v1/unautorized_status
+    Raise:
+        - 401 error
+    """
+    abort(401)
+
 
 @app_views.route('/forbidden', strict_slashes=False)
 def forbidden() -> str:
-  """ GET /api/v1/forbidden
-  Raise:
-    - 403 error
-  """
-  abort(403)
+    """GET /api/v1/forbidden
+    Raise:
+        - 403 error
+    """
+    abort(403)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
