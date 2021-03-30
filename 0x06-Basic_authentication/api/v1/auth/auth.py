c66fc2a5fbd807d42aeb8a078c65898a0e51a56f
arabellak <barbaracalle10i3@gmail.com>
2021-03-29 21:38:08 -0500

    Validations to the path

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,16 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Auth require
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Auth header
