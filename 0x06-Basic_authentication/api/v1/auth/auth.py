a3bcc71b1a065b452b2632f29dbd87156fb12bc4
arabellak <barbaracalle10i3@gmail.com>
2021-03-29 19:19:16 -0500

    added documentation

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -7,13 +7,19 @@ from typing import List, TypeVar
 
 
 class Auth():
-    """
+    """Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Auth require
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """Auth header
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """User
         return None
