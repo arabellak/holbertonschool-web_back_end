df46fb69dfc5d8507cc39022c49a95b092bedc9c
arabellak <barbaracalle10i3@gmail.com>
2021-03-31 14:55:37 -0500

    Imported typing and corrected the fields argument

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-"""Function called filter_datum that returns the log message obfuscated"""
+"""
+Function called filter_datum that returns the log message obfuscated
+"""
 import re
+from typing import List
 
 
-def filter_datum(fields: str, redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """Return - Log message obfuscated"""
     for i in fields:
         message = re.sub(f'{i}=.+?{separator}',
