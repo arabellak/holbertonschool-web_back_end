e6da9b782b368bc84bd06a3ef19c96b9a7dfe53c
arabellak <barbaracalle10i3@gmail.com>
2021-03-31 14:49:13 -0500

    Well documented

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Regex-ing"""
+"""Function called filter_datum that returns the log message obfuscated"""
 import re
 
 
 def filter_datum(fields: str, redaction: str, message: str, separator: str):
-    """Return- Log message obfuscated"""
+    """Return - Log message obfuscated"""
     for i in fields:
         message = re.sub(f'{i}=.+?{separator}',
                          f'{i}={redaction}{separator}', message)
