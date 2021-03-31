3d8e854720a39039dcaa24281756208d0b4e5671
arabellak <barbaracalle10i3@gmail.com>
2021-03-30 20:30:18 -0500

    Validations

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Inherits from Auth"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Base64"""
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) != str:
+            return None
+
+        if authorization_header[:5] != 'Basic ':
+            return authorization_header[6:]
+        else:
+            return None
