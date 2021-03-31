83698293850cfc35dde76e480d9fd51ca238b267
arabellak <barbaracalle10i3@gmail.com>
2021-03-31 14:34:54 -0500

    Regex-ing

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Regex-ing"""
+import re
+
+
+def filter_datum(fields: str, redaction: str, message: str, separator: str):
+    """Return
+        - Log message obfuscated
+    """
+    for i in fields:
+        message = re.sub(f'{i}=.+?{separator}',
+                      f'{i}={redaction}{separator}',
+                      message)
+    return message
