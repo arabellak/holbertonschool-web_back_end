29eca6c7fe9731cd8b4d7cddaf0a119464732081
arabellak <barbaracalle10i3@gmail.com>
2021-03-30 21:46:57 -0500

    halfway there

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,13 +3,14 @@
 Manage the API authentication.
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
     """Inherits from Auth"""
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """Base64"""
+        """Extract"""
         if authorization_header is None:
             return None
 
@@ -19,4 +20,18 @@ class BasicAuth(Auth):
         if authorization_header[:6] == 'Basic ':
             return authorization_header[6:]
         else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                         base64_authorization_header: str) -> str:
+        """Decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            return b64decode(base64_authorization_header)
+        except Exception:
             return None
