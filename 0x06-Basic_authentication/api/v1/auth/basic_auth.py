136d0f9bc7c7f38d9b7e3eb2deff528c348891c3
arabellak <barbaracalle10i3@gmail.com>
2021-03-30 20:31:53 -0500

    Pycode happy

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,8 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Inherits from Auth"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """Base64"""
         if authorization_header is None:
             return None
