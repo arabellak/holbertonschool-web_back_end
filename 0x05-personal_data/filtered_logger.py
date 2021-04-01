86b76f2b04cae682d68d4dfd63d9cfeab5a9325e
bxrbxrx <barbaracalle10i3@gmail.com>
2021-04-01 14:17:16 -0500

    Return value

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -58,7 +58,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Database connector
     """
     user = os.getenv('PERSONAL_DATA_DB_USERNAME')
