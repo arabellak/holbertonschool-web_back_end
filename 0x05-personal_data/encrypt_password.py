6fdd9b72c8808f853b0facdeee0116ecd32b1cfa
bxrbxrx <barbaracalle10i3@gmail.com>
2021-04-01 15:17:33 -0500

    passwor is a string

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ Encrypting passwords
 import bcrypt
 
 
-def hash_password(password) -> bytes:
+def hash_password(password: str) -> bytes:
     """Return
         -  A salted, hashed password, which is a byte string.
     """
