c0fa21ff8a577cee9b654730af374e97f118b978
arabellak <barbaracalle10i3@gmail.com>
2021-03-31 19:58:02 -0500

    Rmd the try

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -73,11 +73,7 @@ class BasicAuth(Auth):
         if user_pwd is None or type(user_pwd) is not str:
             return None
 
-        try:
-            users = User.search({'email': user_email})
-        except Exception
-            return None
-
+        users = User.search({'email': user_email})
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
