f209a8967a94ebab8bb59188f4a32c787a90f063
bxrbxrx <barbaracalle10i3@gmail.com>
2021-04-01 15:31:09 -0500

    Corrected the name

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     password = bytes(password.encode('utf-8'))
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Provided password matches the hashed password
+    """
+    password = bytes(password.encode('utf-8'))
+    return bcrypt.checkpw(password, hashed_password)
