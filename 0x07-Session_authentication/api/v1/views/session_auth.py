378f8b8049e61c1701a6829b476021073db00ac8
arabellak <barbaracalle10i3@gmail.com>
2021-04-07 16:51:46 -0500

    doc

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -10,6 +10,8 @@ from api.v1.views import app_views
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
+    """Login
+    """
     email = request.form.get('email')
     if not email:
         return jsonify({"error": "email missing"}), 400
