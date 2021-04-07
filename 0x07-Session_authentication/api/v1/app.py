318d3411d66b7942296cfcce761ffa236c793d5c
arabellak <barbaracalle10i3@gmail.com>
2021-04-07 15:10:25 -0500

    pycodehappy

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -66,7 +66,7 @@ def before_req():
         return None"""
 
     if auth.authorization_header(request) is None and \
-    auth.session_cookie(request) is None:
+            auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
