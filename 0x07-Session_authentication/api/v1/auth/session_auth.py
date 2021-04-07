2c3e228299f8f297313fa0e2a31f1dcd04f6f01e
arabellak <barbaracalle10i3@gmail.com>
2021-04-07 14:39:21 -0500

    new method

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -21,6 +21,15 @@ class SessionAuth(Auth):
         if user_id is None or type(user_id) is not str:
             return None
         else:
-            session_id = str(uuid.uuid4())
-            self.user_id_by_session_id[session_id] = user_id
-            return session_id
+            sessionId = str(uuid.uuid4())
+            self.user_id_by_session_id[sessionId] = user_id
+            return sessionId
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Return
+             - ID based on a Session ID
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
