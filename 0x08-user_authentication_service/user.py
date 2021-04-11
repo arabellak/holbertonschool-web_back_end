bc97f18ac8435ba237643788b8e90ace150cd1ad
bxrbxrx <barbaracalle10i3@gmail.com>
2021-04-11 18:20:25 -0500

    added the chars limit

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -8,13 +8,14 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
     """Database table user
     """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key="True")
-    email = Column(String)
-    hashed_password = Column(String)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String(250))
+    hashed_password = Column(String(250))
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
