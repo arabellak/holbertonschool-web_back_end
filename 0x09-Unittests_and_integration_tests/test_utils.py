53a912230e7c3fcf44d3dfc360ce198ac1a89017
bxrbxrx <barbaracalle10i3@gmail.com>
2021-04-20 00:26:56 -0500

    Tests nestedmap

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+"""
+import unittest
+from nose.tools import assert_equal
+
+
+class TestAccessNestedMap(TestCase):
+    """Tests access_nested_map function
+    """
+
+    @parameterized.expand([
+        {"a": 1}, path=("a",),
+        {"a": {"b": 2}}, path=("a",),
+        {"a": {"b": 2}}, path=("a", "b"),
+    ])
+    def test_access_nested_map(self, nestedMap, path, expected):
+        """Test that the method returns what it is supposed to
+        """
+        self.assertEqual(access_nested_map(nestedMap, path), expected)
