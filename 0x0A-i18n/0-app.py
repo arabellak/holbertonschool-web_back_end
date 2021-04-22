2bd6cf34a67a463b744f0bdead8bdeb1f35b2b8d
arabellak <barbaracalle10i3@gmail.com>
2021-04-22 16:32:51 -0500

    longer documentation

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -2,14 +2,14 @@
 """
     Flask app
 """
-import app
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_holberton():
-    """Index
+    """Return
+        - template 0-index
     """
     return render_template('0-index.html')
 
