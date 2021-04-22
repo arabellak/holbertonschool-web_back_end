e769984bc14f16409d2851a394ee7b02b0ddffec
arabellak <barbaracalle10i3@gmail.com>
2021-04-22 16:21:09 -0500

    Complted

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -3,10 +3,13 @@
     Flask app
 """
 from app import app
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('0-index')
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    return render_template('0-index')
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
