244e982f93076d38da23f70719dc95de2e7c0731
arabellak <barbaracalle10i3@gmail.com>
2021-04-29 18:09:52 -0500

    enumaration

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- In and not out
+CREATE TABLE IF NOT EXISTS users(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+)
