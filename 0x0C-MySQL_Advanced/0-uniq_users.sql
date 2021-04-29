6c0e98c2fecc41e65985504682a99082e6565790
arabellak <barbaracalle10i3@gmail.com>
2021-04-29 17:48:34 -0500

    Creates the table

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Creates a table users
 
 CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
     PRIMARY KEY (id)
