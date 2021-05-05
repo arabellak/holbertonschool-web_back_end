66601b0b18ff596aea03135eeb4adf3c2727ee3e
arabellak <barbaracalle10i3@gmail.com>
2021-05-04 23:55:56 -0500

    added the return

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -10,7 +10,7 @@ def top_students(mongo_collection):
         Return
          - Sorted students
     """
-    mongo_collection.aggregate([
+    return mongo_collection.aggregate([
         {'$project': {'name': '$name', 'averageScore': {'$avg': '$topics.score'}}},
         {'$sort': {'averageScore': -1}}
     ])
