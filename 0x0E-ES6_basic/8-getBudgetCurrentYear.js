50f4cae216c2934587c856a4be5314072cbd7ca0
arabellak <barbaracalle10i3@gmail.com>
2021-05-06 23:02:27 -0500

    added the return

--- 0x0E-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x0E-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,7 +4,7 @@ function getCurrentYear() {
 }
   
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {
+  const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita
