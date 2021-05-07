b2d5407fa003940b75df4a5f04bb2c313c5e6b5c
arabellak <barbaracalle10i3@gmail.com>
2021-05-06 22:07:12 -0500

    Task 1 to 9

---
+++ 0x0E-ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export default function getNeighborhoodsList() {
+    sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+    const addNeighborhood = let add = newNeighborhood => {
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return self.sanFranciscoNeighborhoods;
+    };
+}
---
+++ 0x0E-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,4 @@
+export default function returnHowManyArguments(...args) {
+    let arrArgs = args
+    return arrArgs.length
+}
