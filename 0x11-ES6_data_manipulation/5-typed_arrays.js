b83dc6ed6b18ad4aa7817b9525bc74b1806af015
arabellak <barbaracalle10i3@gmail.com>
2021-05-18 23:53:48 -0500

    Changed

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
-  if (position < length){ 
+  if (position < length) {
     int8[position] = value;
   } else {
     throw new Error('Position outside range');
