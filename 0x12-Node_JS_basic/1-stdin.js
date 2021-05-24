fd782624404e0f9b1ec204bde4f9d5f6313857ff
arabellak <barbaracalle10i3@gmail.com>
2021-05-23 23:22:55 -0500

    corrected the data and added the end

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,4 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('data', (dat) => {
-  console.log(`Your name is: ${dat}`);
+  process.stdout.write(`Your name is: ${dat}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing \n');
 });
