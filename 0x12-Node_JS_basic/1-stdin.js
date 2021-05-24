229554fa3aaa2c08a9e50e460f57dab79c1a56f0
arabellak <barbaracalle10i3@gmail.com>
2021-05-24 00:11:36 -0500

    Corrected and added the encoding

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
-process.stdin.setEncoding('utf-8')
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (dat) => {
   process.stdout.write(`Your name is: ${dat}`);
