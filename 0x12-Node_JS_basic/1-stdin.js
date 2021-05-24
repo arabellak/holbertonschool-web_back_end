3c76dd957afd72e53845b7c28226c95fc4927f31
arabellak <barbaracalle10i3@gmail.com>
2021-05-23 23:55:17 -0500

    changed the console.logt

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (dat) => {
   process.stdout.write(`Your name is: ${dat}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing \n');
+  process.stdout.write('This important software is now closing');
 });
