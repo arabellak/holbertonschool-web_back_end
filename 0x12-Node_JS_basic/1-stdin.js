8bfb6fd94daae4a9ae24ee2e7bedf8b5094ec3c0
arabellak <barbaracalle10i3@gmail.com>
2021-05-23 23:57:51 -0500

    back to console log

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (dat) => {
   process.stdout.write(`Your name is: ${dat}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing \n');
 });
