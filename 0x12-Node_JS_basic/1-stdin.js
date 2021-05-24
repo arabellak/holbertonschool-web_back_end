77d028d1e7acf11c55c757b8c22bc5c5f1478591
arabellak <barbaracalle10i3@gmail.com>
2021-05-23 22:50:12 -0500

    with eslint

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,6 +1,4 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', function(dat) {
-    console.log(`Your name is: ${dat}`);
-})
-
-
+process.stdin.on('data', (dat) => {
+  console.log(`Your name is: ${dat}`);
+});
