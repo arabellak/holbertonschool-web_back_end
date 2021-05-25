a6b42c02f03f014667d9cc68a2f488020a4d0932
arabellak <barbaracalle10i3@gmail.com>
2021-05-24 23:49:49 -0500

    Corrected

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,11 +1,9 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello Holberton School!');
-    res.end();
-  }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
 }).listen(1245);
 
 module.exports = app;
