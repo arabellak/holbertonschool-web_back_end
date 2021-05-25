3b7d27c0c23de9623cc890171943884f20029150
arabellak <barbaracalle10i3@gmail.com>
2021-05-24 23:58:28 -0500

    Changed

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
 process.stdin.resume();
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', () => {
-  const welcomeName = process.stdin.read();
-  if (welcomeName !== null) {
-    process.stdout.write(`Your name is: ${welcomeName}`);
-  }
+process.stdin.on('data', (welcomeName) => {
+  process.stdout.write(`Your name is: ${welcomeName}`);
 });
 
 process.stdin.on('end', () => {
