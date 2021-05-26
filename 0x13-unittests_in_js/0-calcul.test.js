afe06b0b5a9740fdcd7d093076ab34174f484e0e
arabellak <barbaracalle10i3@gmail.com>
2021-05-26 17:13:53 -0500

    Added args

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,21 @@
-let assert = require('assert');
+const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function() {
+describe('Tests calculateNumber function', () => {
     it('Calculates positive numbers', function() {
-        assert.strictEqual(calculateNumber(1, 3), 4)
-        assert.strictEqual(calculateNumber(1, 3.7), 5)
+        assert.equal(calculateNumber(1, 3), 4)
+        assert.equal(calculateNumber(1, 3.7), 5)
     });
-    it('Calculates negative numbers', function(){
-        assert.strictEqual(calculateNumber(-1, 1), 0)
-        assert.strictEqual(calculateNumber(-48, 8), -40)
+    it('Calculates negative numbers', () => {
+        assert.equal(calculateNumber(-1, 1), 0)
+        assert.equal(calculateNumber(-48, 8), -40)
+        assert.equal(calculateNumber(-26, -77), -103)
     });
-    it('Calculates float numbers', function() {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
-        assert.strictEqual(calculateNumber(-2.8, 9.8), 7)
+    it('Calculates float numbers', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6)
+        assert.equal(calculateNumber(-2.8, 9.8), 7)
     });
+    it('No arguments', () => {
+        assert(isNaN(calculateNumber()));
+    })
 });
