a4d86330b08d23f31f47f089a1143aad465fc683
arabellak <barbaracalle10i3@gmail.com>
2021-05-26 16:56:32 -0500

    Tests calcul.js

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,17 @@
-import { strictEqual } from 'assert';
-import { default as calculateNumber } from './0-calcul';
+let assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
-    it('Calculates given numbers', function() {
-        strictEqual(calculateNumber(1, 3), 4)
-        strictEqual(calculateNumber(1, 3.7), 5)
-        strictEqual(calculateNumber(1.5, 3.7), 6)
+    it('Calculates positive numbers', function() {
+        assert.strictEqual(calculateNumber(1, 3), 4)
+        assert.strictEqual(calculateNumber(1, 3.7), 5)
     });
-    
-})
+    it('Calculates negative numbers', function(){
+        assert.strictEqual(calculateNumber(-1, 1), 0)
+        assert.strictEqual(calculateNumber(-48, 8), -40)
+    });
+    it('Calculates float numbers', function() {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+        assert.strictEqual(calculateNumber(-2.8, 9.8), 7)
+    });
+});
