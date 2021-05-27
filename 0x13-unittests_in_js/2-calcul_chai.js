6ff734881659daf4ad257101f0afba304fa9db2b
arabellak <barbaracalle10i3@gmail.com>
2021-05-26 19:32:52 -0500

    Calculates

--- 0x13-unittests_in_js/2-calcul_chai.js
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -1,5 +1,12 @@
-export default function calculateNumber(type, a, b){
+function calculateNumber(type, a, b){
     if (type === 'SUM') Math.round(a) + Math.round(b);
     if (type === 'SUBTRACT') Math.round(a) - Math.round(b);
-    if (type === 'DIVIDE') Math.round(a) / Math.round(b);
+    if (type === 'DIVIDE') {
+        if (Math.round(b) === 0) {
+            return 'Eror'
+        }
+        Math.round(a) / Math.round(b);
+    }
 }
+
+module.exports = calculateNumber;
