1cc4ce8a70f1bfdb83e4af165891c78b09c0667b
arabellak <barbaracalle10i3@gmail.com>
2021-05-26 19:37:52 -0500

    changed to 2 spaces

--- 0x13-unittests_in_js/2-calcul_chai.js
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -1,12 +1,12 @@
 function calculateNumber(type, a, b){
-    if (type === 'SUM') Math.round(a) + Math.round(b);
-    if (type === 'SUBTRACT') Math.round(a) - Math.round(b);
-    if (type === 'DIVIDE') {
-        if (Math.round(b) === 0) {
-            return 'Eror'
-        }
-        Math.round(a) / Math.round(b);
+  if (type === 'SUM') Math.round(a) + Math.round(b);
+  if (type === 'SUBTRACT') Math.round(a) - Math.round(b);
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Eror'
     }
+    Math.round(a) / Math.round(b);
+  }
 }
 
 module.exports = calculateNumber;
