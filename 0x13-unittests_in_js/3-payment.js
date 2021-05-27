041cd38abdfa1d94a15be632ae7e3570aeb954ff
arabellak <barbaracalle10i3@gmail.com>
2021-05-26 21:22:17 -0500

    payment

--- 0x13-unittests_in_js/3-payment.js
+++ 0x13-unittests_in_js/3-payment.js
@@ -1,4 +1,6 @@
-export default function sendPaymentRequestToApi(totalAmount, totalShipping) {
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
     let total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${total}`);
 }
+
+module.exports = sendPaymentRequestToApi;
