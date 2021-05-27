d07e67573072f234160a0132dd5c26e3a2bfd1f4
arabellak <barbaracalle10i3@gmail.com>
2021-05-26 22:37:14 -0500

    Task 4 to 6

--- 0x13-unittests_in_js/5-payment.js
+++ 0x13-unittests_in_js/5-payment.js
@@ -1,4 +1,8 @@
-export default function sendPaymentRequestToApi(totalAmount, totalShipping) {
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
     let total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${total}`);
 }
+
+module.exports = sendPaymentRequestToApi;
