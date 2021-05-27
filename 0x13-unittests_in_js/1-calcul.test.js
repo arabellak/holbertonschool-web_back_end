ba73907e2142e0c631756617b67cfd7630a5c110
arabellak <barbaracalle10i3@gmail.com>
2021-05-26 19:22:35 -0500

    los pasas

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -2,19 +2,27 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('Tests calculateNumber function', () => {
-    it('Sum', function() {
-        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    });
-    it('Subtract', () => {
-        
-        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    });
-    it('Divide', () => {
-        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.3);
-        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    });
-    it('No arguments', () => {
-        assert(isNaN(calculateNumber()));
-        assert(isNaN(calculateNumber(8)));
+    it("Calculates the sum", () => {
+        it('Sum', () => {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
     })
+    it("Calculates the subtract", () => {
+        it('Subtract', () => {
+        
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+    })
+    it("Calculate the division", () => {
+        it('Divide', () => {
+            assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.3);
+            assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        });
+    it("Arguments", () => {
+        it('No arguments', () => {
+            assert(isNaN(calculateNumber()));
+            assert(isNaN(calculateNumber(8)));
+        });
+    });
+    });
 });
