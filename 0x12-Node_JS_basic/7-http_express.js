20fea4e98b775cc3e5418a0b429aa27eea425da8
arabellak <barbaracalle10i3@gmail.com>
2021-05-28 22:29:04 -0500

    Changed

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -3,23 +3,22 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
-const args = process.argv.slice(2);
-const DATABASE = args[0];
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
-  try {
-    const students = await countStudents(DATABASE);
-    res.send(`${msg}${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${msg}${error.message}`);
-  }
+app.get('/students', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.end(data);
+    })
+    .catch((error) => {
+      res.end(error.message);
+    });
 });
-
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
