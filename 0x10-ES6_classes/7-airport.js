069170bb1255ee6654dff2cdac5b9299250fcf2a
arabellak <barbaracalle10i3@gmail.com>
2021-05-29 21:12:11 -0500

    Corrected the way it was returning

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -1,15 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    if (typeof (name) === 'string') {
-      this._name = name;
-    }
-
-    if (typeof (code) === 'string') {
-      this._code = code;
-    }
+    this._name = name;
+    this._code = code;
   }
 
-  get [Symbol.toString]() {
-    return this._code;
+  toString() {
+    return `[object ${this.code}]`;
   }
 }
