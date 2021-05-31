f0d35b81e4a5afe4df64ae8940f218c9bf3d4ddd
arabellak <barbaracalle10i3@gmail.com>
2021-05-30 23:09:36 -0500

    task 4 & 6

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,32 +1,18 @@
+const expect = require('chai').expect;
 const sinon = require('sinon');
-const expect = chai.expect;
-const chai = require('chai');
-const sendPaymentRequestToApi = require('./4-payment');
-const Utils = require('./utils');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
+describe('sendPaymentRequestToApi function', () => {
+  const consoleSpy = sinon.spy(console, 'log');
 
-describe('Tests sendPaymentRequestToApi function', function() {
-    let spies;
-    beforeEach(() => {
-        spies = sinon.spy(console, 'log');
-    });
-    afterEach(() => {
-        spies.restore();
-    })
-    it('validate the usage of the Utils function', function() {
-      
-      const calcStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-
-      sendPaymentRequestToApi(100, 20);
-
-      expect(spies.calledOnce).to.be.true;
-      expect(spies.calledWith('The total is: 10')).to.be.true;
-
-      expect(calcStub.calledOnce).to.be.true;
-      expect(calcStub.args[0][0]).to.equal('SUM');
-      expect(calcStub.args[0][1]).to.equal(100);
-      expect(calcStub.args[0][2]).to.equal(20);
-   
-      calcStub.restore();
-    });
+  it('validate the usage of the Utils function', () => {
+    const utilStub = sinon.stub(Utils, 'calculateNumber');
+    utilStub.withArgs('SUM', 100, 20).returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    utilStub.restore()
+    consoleSpy.restore();
+  });
 });
--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -5,8 +5,10 @@ const expect = chai.expect;
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('Tests getPaymentTokenFromAPI function', function() {
-  it('getting resolved promise', function(done) {
-    const promise = getPaymentTokenFromAPI(true);
+  it('return a resolve promise', function(done) {
+    getPaymentTokenFromAPI(true).then((res) => {
+      expect(res).to.eql({data: 'Successful response from the API' })
+    })
     done();
   });
 });
