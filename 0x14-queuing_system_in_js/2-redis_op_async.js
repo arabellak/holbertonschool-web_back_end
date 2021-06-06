39c2c4736e08eb3e2ca0f7082d65042576f1459f
arabellak <barbaracalle10i3@gmail.com>
2021-06-06 01:13:48 -0500

    From task 0 to 4

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,25 @@
+const redis = require('redis');
+const client = redis.createClient();
+const { promisify } = require('util');
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const getAsync = promisify(client.get).bind(client);
+  const asyncPromise = await getAsync(schoolName);
+  console.log(asyncPromise);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
